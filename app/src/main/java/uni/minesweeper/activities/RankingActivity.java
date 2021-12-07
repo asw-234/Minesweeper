a505031f589f6a688f9cfefc59973853f14cf8df
Ashwin <testmester1337@gmail.com>
2021-12-08 00:19:38 +0100

    Style leaderboard rankings

--- app/src/main/java/uni/minesweeper/activities/RankingActivity.java
+++ app/src/main/java/uni/minesweeper/activities/RankingActivity.java
@@ -1,6 +1,5 @@
 package uni.minesweeper.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +7,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import uni.minesweeper.R;
-import uni.minesweeper.Utils;
-import uni.minesweeper.activities.play.IntroActivity;
 import uni.minesweeper.adapter.RecyclerAdapter;
 import uni.minesweeper.database.FirebaseManager;
+
 
 public class RankingActivity extends AppCompatActivity {
   private RecyclerView recyclerView;
@@ -24,14 +22,11 @@ public class RankingActivity extends AppCompatActivity {
 
     recyclerView = findViewById(R.id.recyclerID);
     recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     FirebaseManager.getInstance().fetchRankingUsers(rankingList -> {
       recyclerAdapter = new RecyclerAdapter(RankingActivity.this, rankingList);
       recyclerView.setAdapter(recyclerAdapter);
     });
-
-    findViewById(R.id.ibBack).setOnClickListener(view ->
-      Utils.sendToActivity(this, IntroActivity.class, Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)
-    );
   }
 
   @Override
