19981306622a2800af8f27ebd8090a06dccc1d93
Ashwin <testmester1337@gmail.com>
2021-12-07 23:57:06 +0100

    Finish menu functionality

--- app/src/main/java/uni/minesweeper/MenuActivity.java
+++ app/src/main/java/uni/minesweeper/MenuActivity.java
@@ -1,5 +1,6 @@
 package uni.minesweeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -13,8 +14,12 @@ public class MenuActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_menu);
 
-    findViewById(R.id.btnPlay).setOnClickListener(view -> {
+    findViewById(R.id.btnPlay).setOnClickListener(view -> Utils.sendToActivity(this, IntroActivity.class, 0));
+    findViewById(R.id.btnLeaderboard).setOnClickListener(view -> Utils.sendToActivity(this, RankingActivity.class, 0));
+    findViewById(R.id.btnLogout).setOnClickListener(view -> logout());
+  }
 
-    });
+  private void logout() {
+    Utils.sendToActivity(this, LoginActivity.class, Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
   }
 }
